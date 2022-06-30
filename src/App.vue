0d8c6986aed6043e95dbb57989004a6c3f5487cd
Abel Arbildo <abel.arbildo.2019@gmail.com>
2022-06-29 20:28:08 -0700

    Fix: Order the last form to communicate the user about the form was send successfully

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
-  <select name="tickets" id="tickets" :value="$store.state.tickets" @input="handleSelect">
-    <option value="0" disabled selected="selected"> Choose an Option</option>
-    <option v-for="number in $store.state.maxTickets" :key="number" :value="number"> {{ number }}</option>
-  </select>
-  <div v-if="$store.state.users.length > 0">
-    <finish-form v-if="$store.state.continue === 'finish'"></finish-form>
-    <form @submit="handleSubmit" v-else>
-      <div v-if="$store.state.continue === 'review'" >
-          <div id="review">
-              <div class="edit-box">
-                  <the-button type-button="button" name="<" @click="handleBack" class="edit-btn"></the-button>
-              </div>
-              <h2>review</h2>
-          </div>
+  <finish-form v-if="$store.state.continue === 'finish'"></finish-form>
+  <div v-else>
+    <select name="tickets" id="tickets" :value="$store.state.tickets" @input="handleSelect">
+      <option value="0" disabled selected="selected"> Choose an Option</option>
+      <option v-for="number in $store.state.maxTickets" :key="number" :value="number"> {{ number }}</option>
+    </select>
+    <div v-if="$store.state.users.length > 0">
+      <form @submit.prevent="handleSubmit">
+        <div v-if="$store.state.continue === 'review'" >
+            <div id="review">
+                <div class="edit-box">
+                    <the-button type-button="button" name="<" @click="handleBack" class="edit-btn"></the-button>
+                </div>
+                <h2>review</h2>
+            </div>
+            <ul>
+              <li v-for="ticket in getTickets()" :key="ticket" id="form">
+                <the-registration :user="$store.state.users" :id="parseInt(ticket)"></the-registration>
+              </li>
+            </ul>
+            <div class="submit-box">
+                <the-button type-button="submit" :is-disabled="enableSubmit" name="submit"></the-button>
+            </div>
+        </div>
+        <div v-else>
           <ul>
-            <li v-for="ticket in getTickets()" :key="ticket" id="form">
-              <the-registration :user="$store.state.users" :id="parseInt(ticket)"></the-registration>
+            <li v-for="ticket in getTickets()" :key="ticket">
+              <multi-form :user="$store.state.users" :id="parseInt(ticket)"></multi-form>
             </li>
           </ul>
-          <div class="submit-box">
-              <the-button type-button="submit" :is-disabled="enableSubmit" name="submit"></the-button>
+          <div>
+            <the-button :is-disabled="enableSubmit" type-button="button" name="next" @click="handleNext"></the-button>
+            <p>{{ this.$store.state.validMultiForm }}</p>
           </div>
-      </div>
-      <div v-else>
-        <ul>
-          <li v-for="ticket in getTickets()" :key="ticket">
-            <multi-form :user="$store.state.users" :id="parseInt(ticket)"></multi-form>
-          </li>
-        </ul>
-        <div>
-          <the-button :is-disabled="enableSubmit" type-button="button" name="next" @click="handleNext"></the-button>
-          <p>{{ this.$store.state.validMultiForm }}</p>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
 
