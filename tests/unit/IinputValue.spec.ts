ab323f686985f9e08cc0fffb36535ec4b7ab4d87
Abel Arbildo <abel.arbildo.2019@gmail.com>
2022-07-07 07:50:43 -0700

    Fix: Testing InputValue component when the input is type text and when this input is valid or invalid for name and lastname

--- tests/unit/IinputValue.spec.ts
+++ tests/unit/IinputValue.spec.ts
@@ -1,3 +1,4 @@
+import store from '@/store';
 import { mount } from '@vue/test-utils';
 import InputField from '../../src/components/UI/InputField.vue';
 
@@ -20,6 +21,7 @@ describe('Input field', () => {
     });
 
     const inputValue = wrapper.find('input[type="text"]');
+    const regex = store.state.regex;
 
 
     test('The default Slot in InputField will be Abel Antonio', async () => { 
@@ -27,5 +29,25 @@ describe('Input field', () => {
         await inputValue.setValue('Abel Antonio');
 
         expect((inputValue.element as HTMLInputElement).value).toBe('Abel Antonio');
+    });
+
+    test('Should return Invalid Field', async () => { 
+
+        await inputValue.setValue('Abel Antonio11');
+
+        const isNotValid = mount(InputField, {...wrapper, props: {...wrapper.props(), validation: true}})
+        const notValid = regex.test((inputValue.element as HTMLInputElement).value);
+
+        expect(notValid).toBe(false);
+        expect(isNotValid.find('p').element.textContent).toContain('invalid field');
+     });
+
+    test('should first', async () => { 
+        await inputValue.setValue('Abel John');
+        const valid = regex.test((inputValue.element as HTMLInputElement).value);
+        const test = mount(InputField, {...wrapper, slots:{ default: '<input placeholder="Name" id="name" type="text" v-model="valueName"/>', valid: '<p>valid field</p>' }, props: {...wrapper.props()}})
+
+        expect(valid).toBe(true);
+        expect(test.find('p').element.textContent).toContain('valid fiel');
     });
  }) 
