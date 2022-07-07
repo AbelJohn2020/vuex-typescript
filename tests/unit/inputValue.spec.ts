a3ba5d6ec8f56485084914c85b8d76db69290b39
Abel Arbildo <abel.arbildo.2019@gmail.com>
2022-07-07 09:33:21 -0700

    Fix: Add Testinig for InputValue componnent but in case it will be called Nationality

--- tests/unit/inputValue.spec.ts
+++ tests/unit/inputValue.spec.ts
@@ -2,7 +2,7 @@ import store from '@/store';
 import { mount } from '@vue/test-utils';
 import InputField from '../../src/components/UI/InputField.vue';
 
-describe('Input field', () => { 
+describe('Input field Name', () => { 
     const wrapper = mount(InputField, {
         slots: {
             default: `
@@ -61,6 +61,65 @@ describe('Input field', () => {
         expect(test.find('p').element.textContent).toContain('valid fiel');
         expect(test.html()).toMatchSnapshot();
     });
+ });
 
 
- }) 
+describe('Input Field Nationality', () => { 
+    const wrapper = mount(InputField, {
+        slots: {
+            default: `
+                <input placeholder="Name" id="name" type="text" v-model="valueNationality"/>
+            `,
+        },
+        props: {
+            forLabel: 'nationality',
+            validation: false
+        },
+        data() {
+            return {
+                valueNationality: '',
+            };
+        },
+    });
+
+    const inputValue = wrapper.find('input[type="text"]');
+    const regex = store.state.regNationality;
+
+
+    test('The default Slot in InputField will be Peruano', async () => { 
+
+        await inputValue.setValue('Peruano');
+        expect((inputValue.element as HTMLInputElement).value).toBe('Peruano');
+    });
+
+    test('Should return Invalid Field', async () => { 
+
+        await inputValue.setValue("Peruano'");
+
+        const isNotValid = mount(InputField, {
+            ...wrapper, 
+            slots:{ default: '<input placeholder="Name" id="name" type="text" v-model="valueNationality"/>' },
+            props: {...wrapper.props(), validation: true}
+        });
+
+        const notValid = regex.test((inputValue.element as HTMLInputElement).value);
+
+        expect(notValid).toBe(false);
+        expect(isNotValid.find('p').element.textContent).toContain('invalid field');
+        expect(isNotValid.html()).toMatchSnapshot();
+     });
+
+    test('should return Valid Field', async () => { 
+        await inputValue.setValue('peruano');
+        const valid = regex.test((inputValue.element as HTMLInputElement).value);
+        const test = mount(InputField, {
+            ...wrapper, 
+            slots:{ default: '<input placeholder="Name" id="name" type="text" v-model="valueNationality"/>', valid: '<p>valid field</p>' }, 
+            props: {...wrapper.props()}
+        });
+
+        expect(valid).toBe(true);
+        expect(test.find('p').element.textContent).toContain('valid fiel');
+        expect(test.html()).toMatchSnapshot();
+    });
+  })
